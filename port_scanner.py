b77451085aeb69df9930b47e70ec633a2debbd28
Danish Shaikh <dskhaikh777@gmail.com>
2025-07-23 19:01:56 +0530

    feat: Implement command-line argument parsing

--- port_scanner.py
+++ port_scanner.py
@@ -3,7 +3,18 @@ import argparse
 import sys
 
 def main():
-    pass
+    # parser object
+    parser = argparse.ArgumentParser(description="A simple network port scanner")
+
+    # arguments
+    parser.add_argument("host", type=str, help="The IP address or hostname to scan")
+    parser.add_argument("-p", "--ports", type=str, default="1-1024", help="Port range to scan (e.g., 1-1024, 80, 22)")
+
+    #parsing the argument from cmd line
+    args = parser.parse_args()
+
+    print(f"Target:{args.host}, Port: {args.ports}")
+
 
 if __name__ == "__main__":
     main()
